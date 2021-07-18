cae6d8dfb82c3e9760ecc4496078d8c2362ef244
Kunal Kundu <kunal99kundu@gmail.com>
2021-07-18 15:06:20 +0530

    adds functions to interact with the parser and handle errors

--- main.go
+++ main.go
@@ -1,19 +1,64 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strings"
 )
+
+var hadError = false
 
 func main() {
 	args := os.Args[1:]
 	switch len(args) {
 	case 0:
-		// Start interactive session
+		runPrompt()
 	case 1:
-		// Read file
+		runFile(args[0])
 	default:
 		fmt.Println("Usage: glin <file-name>")
 		os.Exit(64)
 	}
+}
+
+func runFile(path string) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+	run(string(data))
+	if hadError {
+		os.Exit(65)
+	}
+}
+
+func runPrompt() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("> ")
+		line, _, err := reader.ReadLine()
+		if err != nil || len(line) == 0 {
+			break
+		}
+		run(string(line))
+		hadError = false
+	}
+}
+
+func run(source string) {
+	tokens := strings.Split(source, " ")
+	for _, token := range tokens {
+		fmt.Println(token)
+	}
+}
+
+func error(line int, message string) {
+	report(line, "", message)
+}
+
+func report(line int, where string, message string) {
+	fmt.Printf("[line %v] Error %s: %s", line, where, message)
+	hadError = true
 }
