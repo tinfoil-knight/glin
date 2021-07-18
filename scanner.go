764c8a5993377e4c4140a7a47e24782f1822d6a1
Kunal Kundu <kunal99kundu@gmail.com>
2021-07-18 19:14:51 +0530

    adds support for more token in scanToken

--- scanner.go
+++ scanner.go
@@ -47,10 +47,41 @@ func (tl *TokenList) scanToken() {
 		';': SEMICOLON,
 		'*': STAR,
 	}
+	multiCharLexemes := map[byte][]TokenType{
+		'!': {BANG_EQUAL, BANG},
+		'=': {EQUAL_EQUAL, EQUAL},
+		'<': {LESS_EQUAL, LESS},
+		'>': {GREATER_EQUAL, GREATER},
+	}
+
 	if v, ok := singleCharLexemes[c]; ok {
 		tl.addToken(v, "")
+		return
 	}
-	error(tl.line, "Unexpected character.")
+	if v, ok := multiCharLexemes[c]; ok {
+		if tl.match('=') {
+			tl.addToken(v[0], "")
+		} else {
+			tl.addToken(v[1], "")
+		}
+		return
+	}
+	switch c {
+	case '/':
+		if tl.match('/') {
+			for tl.peek() != '\n' && !tl.isAtEnd() {
+				tl.advance()
+			}
+		} else {
+			tl.addToken(SLASH, "")
+		}
+	case '\n':
+		tl.line++
+	case ' ', '\r', '\t':
+		// ignored
+	default:
+		error(tl.line, "Unexpected character.")
+	}
 }
 
 func (tl *TokenList) advance() byte {
@@ -62,4 +93,19 @@ func (tl *TokenList) addToken(typ TokenType, literal string) {
 	text := tl.source[tl.start:tl.current]
 	newToken := Token{typ, text, literal, tl.line}
 	tl.tokens = append(tl.tokens, newToken)
+}
+
+func (tl *TokenList) match(expected byte) bool {
+	if tl.isAtEnd() || tl.source[tl.current] != expected {
+		return false
+	}
+	tl.current++
+	return true
+}
+
+func (tl *TokenList) peek() byte {
+	if tl.isAtEnd() {
+		return '\000'
+	}
+	return tl.source[tl.current]
 }
