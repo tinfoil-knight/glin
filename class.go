11952c55c7020a7874290d57adc0206a3d144154
Kunal Kundu <kunal99kundu@gmail.com>
2021-11-13 18:00:45 +0530

    adds support for empty class instances

--- class.go
+++ class.go
@@ -1,5 +1,6 @@
 package main
 
+// LoxClass implements LoxCallable
 type LoxClass struct {
 	name string
 }
@@ -8,6 +9,23 @@ func NewLoxClass(name string) *LoxClass {
 	return &LoxClass{name}
 }
 
-func (l LoxClass) String() string {
+func (l *LoxClass) arity() int {
+	return 0
+}
+
+func (l *LoxClass) call(_ *Interpreter, _ []interface{}) interface{} {
+	instance := LoxInstance{*l}
+	return instance
+}
+
+func (l *LoxClass) String() string {
 	return l.name
+}
+
+type LoxInstance struct {
+	class LoxClass
+}
+
+func (l LoxInstance) String() string {
+	return l.class.name + " instance"
 }
--- function.go
+++ function.go
@@ -1,5 +1,6 @@
 package main
 
+// LoxFunction implements LoxCallable
 type LoxFunction struct {
 	declaration Function
 	closure     Environment
